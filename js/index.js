ec1da31f0e8a32c73cc3d4b87da58ccf3d945d89
tim <lonelyblue89@gmail.com>
2022-04-21 18:04:06 +0700

    update on responsive

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".navbar-toggle");
+const menuToggle = document.querySelector(".navbar");
+
+toggle.addEventListener('click', ({
+    menuToggle.classList.add('.active');
+
+}));
