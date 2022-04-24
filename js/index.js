d328ab2d3a6a1f4097a6e21ab91fb16962d4c807
timfung <thinhpt.sp@gmail.com>
2022-04-24 22:54:35 +0700

    add btn to top

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-const toggle = document.querySelector(".navbar-toggle");
-const menu = document.querySelector(".navbar");
-
 window.addEventListener("load", (event) => {
+  const toggle = document.querySelector(".navbar-toggle");
+  const menu = document.querySelector(".navbar");
+  const toTop = document.querySelector(".footer-to-top");
+
+  // toggle menu
   toggle.addEventListener("click", function () {
     menu.classList.add("active");
   });
@@ -10,5 +12,17 @@ window.addEventListener("load", (event) => {
     if (!menu.contains(e.target) && !e.target.matches(".navbar-toggle")) {
       menu.classList.remove("active");
     }
+  });
+
+  // to top
+
+  toTop.addEventListener("click", function () {
+    console.log("ok");
+
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   });
 });
